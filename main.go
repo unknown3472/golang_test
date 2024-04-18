556f1c0ab61ef24f615169f29e103c50b18769be
Madina <jumaniyozovamadina71@gmail.com>
2024-04-18 13:52:14 +0500

    uy ishi

--- main.go
+++ main.go
@@ -1,61 +1,32 @@
 package main
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"encoding/json"
+	"log"
+	"os"
 )
-type User struct {
-    Name       string
-    Age        int
-    Occupation string
+type Employee struct{
+	Id int `json:"id"`
+	First_name string `json:"name"`
+	Last_name string `json:"last_name"`
+	Email string `json:"email"`
+	Age int `json:"age"`
 }
-
-func main() {
-    file, err := os.Open("sample.txt")
-    if err != nil {
-        fmt.Println("Error opening file:", err)
-        return
-    }
-    defer file.Close()
-    scanner := bufio.NewScanner(file)
-    var users []User
-    var name, occupation string
-    var age int
-    for scanner.Scan() {
-        line := scanner.Text()
-        if line == "" {
-            if name != "" {
-                users = append(users, User{Name: name, Age: age, Occupation: occupation})
-                name, occupation = "", ""
-                age = 0
-            }
-        } else {
-            parts := strings.Split(line, ":")
-            if len(parts) == 2 {
-                key := strings.TrimSpace(parts[0])
-                value := strings.TrimSpace(parts[1])
-
-                switch key {
-                case "Name":
-                    name = value
-                case "Age":
-                    fmt.Sscanf(value, "%d", &age)
-                case "Occupation":
-                    occupation = value
-                }
-            }
-        }
-    }
-    if name != "" {
-        users = append(users, User{Name: name, Age: age, Occupation: occupation})
-    }
-    for _, user := range users {
-        fmt.Printf("Name: %s, Age: %d, Occupation: %s\n", user.Name, user.Age, user.Occupation)
-    }
-    if err := scanner.Err(); err != nil {
-        fmt.Println("Error scanning file:", err)
-    }
+func main(){
+	var e []Employee
+	f, _ := os.ReadFile("employees.json")
+	json.Unmarshal(f, &e)
+	for j, i := range e{
+		if i.Id==3{
+			e[j].Age=50
+		}
+		if i.Id==6{
+			e[j].Email, e[j].First_name, e[j].Last_name = "johndoe@gmail.com", "John", "Doe"
+			e[j].Age= 100
+		}
+	}
+	d, _ := json.MarshalIndent(e, "", " ")
+	err := os.WriteFile("employees_new.json", d, 0644)
+	if err!= nil{
+		log.Fatal(err)
+	}
 }
-
-
