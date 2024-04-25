2e0792bb03ac076fe5577b7ad184c9e3ec7ff7d6
Madina <jumaniyozovamadina71@gmail.com>
2024-04-25 13:29:55 +0500

    main

---
+++ git/git.go
@@ -0,0 +1,21 @@
+package git
+
+import (
+	"os/exec"
+)
+func GetUserName() (string, error) {
+	cmd := exec.Command("git", "config", "--global", "user.name")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+func GetUserEmail() (string, error) {
+	cmd := exec.Command("git", "config", "--global", "user.email")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
--- main.go
+++ main.go
@@ -1,77 +1,41 @@
-//                          uy ishi-1
-// package main
+package main
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
-// func factCalc(num int, ch chan int) {
-// 	fact := 1
-// 	for i := 2; i <= num; i++ {
-// 		fact *= i
-// 	}
-// 	ch <- fact
-// }
+	"github.com/unknown3472/golang_test.git/golang_test/git"
+)
 
-// func main() {
-// 	numbers := []int{5, 6, 7, 8, 9}
-// 	results := make(chan int)
-// 	for _, num := range numbers {
-// 		go factCalc(num, results)
-// 	}
-// 	for i := 0; i < len(numbers); i++ {
-// 		fmt.Printf("Faktoriali %d: %d\n", numbers[i], <-results)
-// 	}
-// 	close(results)
-// }
+func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	filePath := filepath.Join(dir, "userinfo.txt")
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	username, err := git.GetUserName()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = file.WriteString(fmt.Sprintf("Username: %s\n", username))
+	if err != nil {
+		log.Fatal(err)
+	}
+	email, err := git.GetUserEmail()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = file.WriteString(fmt.Sprintf("Email: %s\n", email))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-
-
-
-
-//                                 uy ishi-2
-// package main
-
-// import (
-// 	"fmt"
-// 	"os"
-// )
-
-// func main() {
-// 	files := []string{"file1.txt", "file2.txt", "file3.txt"}
-// 	var mergedContent []byte
-// 	for _, filename := range files {
-// 		file, err := os.Open(filename)
-// 		if err != nil {
-// 			fmt.Printf("Xatolik: %v\n", err)
-// 			continue
-// 		}
-// 		defer file.Close()
-
-// 		fileInfo, err := file.Stat()
-// 		if err != nil {
-// 			fmt.Printf("Xatolik: %v\n", err)
-// 			continue
-// 		}
-
-// 		fileSize := fileInfo.Size()
-// 		content := make([]byte, fileSize)
-
-// 		_, err = file.Read(content)
-// 		if err != nil {
-// 			fmt.Printf("Xatolik: %v\n", err)
-// 			continue
-// 		}
-
-// 		mergedContent = append(mergedContent, content...)
-// 	}
-// 	fmt.Println(string(mergedContent))
-// 	err := os.WriteFile("merged_file.txt", mergedContent, os.ModePerm)
-// 	if err != nil {
-// 		fmt.Printf("Xatolik: %v\n", err)
-// 		return
-// 	}
-// 	fmt.Println("Faylga muvaffaqiyatli yozildi: merged_file.txt")
-// }
-
-
+	fmt.Println("User information has been written to userinfo.txt successfully.")
+}
